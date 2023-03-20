6fc1e249d1331ece01b9e4bcceae6c3a85b81aad
pitzzahh <pitzzahh@araopj.tech>
2023-03-20 21:11:05 +0800

    add: Account class model

---
+++ app/src/main/java/org/russkyc/androidappmvvm/backend/entity/Account.java
@@ -0,0 +1,46 @@
+package org.russkyc.androidappmvvm.backend.entity;
+
+import com.orm.SugarRecord;
+import java.util.Objects;
+
+public class Account extends SugarRecord {
+    private String username;
+    private String password;
+
+    public Account() {
+    }
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return username.equals(account.username) && password.equals(account.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+}
